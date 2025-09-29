47d81ace6970ae75aebc221c34311877a7bcecfc
paststar <ladycar96@gmail.com>
2025-09-29 13:05:10 +0900

    chore: add temperature for llm & fix daily word prompt

--- airflow/dags/common/llm/gemini.py
+++ airflow/dags/common/llm/gemini.py
@@ -5,10 +5,11 @@ import common.configs as configs
 import common.utils as utils
 
 
-def generation_text(prompt: str, response_schema) -> dict:
+def generation_text(prompt: str, response_schema: list, temperature: float = 1.0) -> dict:
     client = genai.Client(api_key=configs.GEMINI_API_KEY)
     config = types.GenerateContentConfig(
         response_schema=response_schema,
+        temperature=temperature,
     )
     resp = client.models.generate_content(
         model=configs.GEMINI_MODEL,
@@ -18,11 +19,12 @@ def generation_text(prompt: str, response_schema) -> dict:
     return utils.gemini_resp_to_dict(resp.text)
 
 
-def generate_text_with_search(prompt: str, response_schema) -> dict:
+def generate_text_with_search(prompt: str, response_schema: list, temperature: float = 1.0) -> dict:
     client = genai.Client(api_key=configs.GEMINI_API_KEY)
     grounding_tool = types.Tool(google_search=types.GoogleSearch())
     config = types.GenerateContentConfig(
         response_schema=response_schema,
+        temperature=temperature,
         tools=[grounding_tool],
     )
     resp = client.models.generate_content(
@@ -34,15 +36,16 @@ def generate_text_with_search(prompt: str, response_schema) -> dict:
 
 
 if __name__ == "__main__":
+    from pprint import pprint
+    
     import answer_recommendation.candidate_generation as cg
     import common.llm.response_schemas as rs
 
-    PROMPT_PATH = "common/llm/prompts/daily_words.md"
 
-    prompt = utils.load_and_fill_prompt(PROMPT_PATH, cg.get_korean_season("2025-09-25"))
-    # resp = generation_text(
-    #     prompt, response_schema=list[rs.AnswerCandidate]
-    # )
-    resp = generate_text_with_search(prompt, response_schema=list[rs.AnswerCandidate])
-    print(resp)
-    # print(prompt)
+    # resp = generation_text(prompt, response_schema=list[rs.AnswerCandidate])
+     
+    prompt = utils.load_and_fill_prompt("common/llm/prompts/daily_words.md", cg.get_korean_season("2025-09-29"))
+    resp = generate_text_with_search(prompt, response_schema=list[rs.AnswerCandidate], temperature=2.0)
+    
+    pprint(resp)
+    
